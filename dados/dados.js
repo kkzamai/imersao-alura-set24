196e64656ad6b13c96de7a33aeeafdc709cacfab
Kristiany Kukert Zamai <kkzamai@gmail.com>
2024-09-09 21:15:01 -0300

    saving code

---
+++ dados/dados.js
@@ -0,0 +1,76 @@
+const dados = [
+{
+	titulo: 'Ametista',
+	descricao: 'A ametista é uma variedade de quartzo que apresenta coloração violeta, atribuída à presença de ferro em sua composição. É uma pedra preciosa muito apreciada por sua beleza e propriedades místicas.',
+	linkWikipedia: 'https://pt.wikipedia.org/wiki/Ametista'
+ },
+ {
+	titulo: 'Diamante',
+	descricao: 'O diamante é a forma alotrópica cristalina do carbono, sendo a substância natural mais dura conhecida. É amplamente utilizado em joalheria.',
+	linkWikipedia: 'https://pt.wikipedia.org/wiki/Diamante'
+ },
+ {
+	titulo: 'Esmeralda',
+	descricao: 'A esmeralda é uma variedade verde vívida do mineral berilo. É uma das quatro pedras preciosas mais valiosas, junto com o rubi, o safira e o diamante.',
+	linkWikipedia: 'https://pt.wikipedia.org/wiki/Esmeralda'
+ },
+{
+    titulo: 'Rubi',
+    descricao: 'O rubi é uma variedade vermelha da espécie mineral coríndon. É uma das quatro pedras preciosas mais valiosas, junto com o diamante, a esmeralda e a safira.',
+    linkWikipedia: 'https://pt.wikipedia.org/wiki/Rubi'
+},
+{
+    titulo: 'Opala',
+    descricao: 'A opala é uma pedra preciosa opaca, composta por sílica amorfa e água. Apresenta uma ampla gama de cores e padrões.',
+    linkWikipedia: 'https://pt.wikipedia.org/wiki/Opala'
+ },
+ {
+    titulo: 'Turmalina',
+    descricao: 'A turmalina é um grupo de minerais boratos que apresentam uma ampla variedade de cores. É frequentemente utilizada em joias.',
+    linkWikipedia: 'https://pt.wikipedia.org/wiki/Turmalina'
+ },
+{
+    titulo: 'Safira',
+    descricao: 'A safira é uma variedade do mineral coríndon. É conhecida por suas cores vibrantes, especialmente o azul, mas pode ser encontrada em diversas outras tonalidades.',
+    linkWikipedia: 'https://pt.wikipedia.org/wiki/Safira'
+},
+{
+    titulo: 'Topázio',
+    descricao: 'O topázio é um mineral silicato de alumínio e flúor. É conhecido por sua variedade de cores, incluindo amarelo, azul, rosa e incolor.',
+    linkWikipedia: 'https://pt.wikipedia.org/wiki/Topázio'
+},
+{
+    titulo: 'Peridoto',
+    descricao: 'O peridoto é uma variedade verde oliva do mineral olivina. É uma pedra preciosa relativamente rara.',
+    linkWikipedia: 'https://pt.wikipedia.org/wiki/Peridoto'
+},
+{
+    titulo: 'Granada',
+    descricao: 'A granada é um grupo de minerais que formam cristais com hábito isométrico. Apresenta uma ampla variedade de cores, incluindo vermelho, verde, amarelo e marrom.',
+    linkWikipedia: 'https://pt.wikipedia.org/wiki/Granada'
+},
+{
+    titulo: 'Tanzanita',
+    descricao: 'A tanzanita é uma variedade azul de zoisita, encontrada principalmente na Tanzânia. É uma pedra preciosa relativamente nova e bastante valorizada.',
+    linkWikipedia: 'https://pt.wikipedia.org/wiki/Tanzanita'
+},
+{
+    titulo: 'Morganita',
+    descricao: 'A morganita é uma variedade rosa ou vermelho-alaranjada do mineral berilo. É uma pedra preciosa relativamente rara e valorizada.',
+    linkWikipedia: 'https://pt.wikipedia.org/wiki/Morganita'
+},
+{
+    titulo: 'Larimar',
+    descricao: 'O larimar é uma pedra preciosa azul turva encontrada exclusivamente na República Dominicana. É composta principalmente por pectolita.',
+    linkWikipedia: 'https://pt.wikipedia.org/wiki/Larimar'
+},
+{
+    titulo: 'Jade',
+    descricao: 'O jade é um nome comum para duas diferentes pedras preciosas: jadeíta e nefrita. Ambas são altamente valorizadas por sua dureza e beleza.',
+    linkWikipedia: 'https://pt.wikipedia.org/wiki/Jade'
+},
+{
+    titulo: 'Ágata',
+    descricao: 'A ágata é uma variedade microcristalina de quartzo. Apresenta uma ampla variedade de cores e padrões, sendo frequentemente utilizada em joias e objetos decorativos.',
+    linkWikipedia: 'https://pt.wikipedia.org/wiki/Ágata'
+}]
